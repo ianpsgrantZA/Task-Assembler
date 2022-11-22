50b41d7727bad27ff4dab62645fc805e24b91efc
ianpsgrantZA <ianpsgrant@gmail.com>
2022-11-22 20:04:22 +0200

    read from stdin

--- 1947-Assembly/README.md
+++ 1947-Assembly/README.md
@@ -10,7 +10,6 @@
 
 ### Running instructions
 ```
-nasm -f elf64 -o hello.o hello.s
-ld -o hello hello.o
-./hello
+make build
+make run
 ```
--- 1947-Assembly/src/main.asm
+++ 1947-Assembly/src/main.asm
@@ -4,19 +4,30 @@ global _start
 
 section .text
 _start:
-  mov rax, 1        ; write syscall
-  mov rdi, 1        ; use stdout as file descriptor
-  mov rsi, msg1     ; use message as buffer
-  mov rdx, msg1len  ; supply message length
+  mov rax, 1          ; write syscall
+  mov rdi, 1          ; use stdout as file descriptor
+  mov rsi, msg1       ; use message as buffer
+  mov rdx, msg1_len   ; supply message length
   syscall
 
-  mov rax, 0x3C     ; exit syscall
-  mov rdi, 0        ; exit gracefully
+  mov rax, 0          ; read syscall
+  mov rdi, 0          ; use stdin as file descriptor
+  mov rsi, buffer     ; rsi <- address of the buffer.  lea rsi, [rel buffer]
+  mov edx, BUFSIZE    ; rdx <- size of the buffer
+
+  mov rax, 0x3C       ; exit syscall
+  mov rdi, 0          ; exit gracefully
   syscall
 
-section .rodata     ; .rodata for constants, .data for non-constants
+section .bss          ; uninitialised data
+  buffer:
+  buffer_len:
+
+section .data         ; initialised data
+
+section .rodata       ; constants data
   msg1: db "Hello, world!", 0xA
-  msg1len: equ $-msg1
+  msg1_len: equ $-msg1
 
 
 ; syscalls: https://blog.rchapman.org/posts/Linux_System_Call_Table_for_x86_64/
